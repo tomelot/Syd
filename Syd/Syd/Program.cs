4f06ec00fd9b032d980233dcbe90fed24514ed0d
tomelot <tomlot2012@gmail.com>
2017-07-08 01:38:20 +0300

    gh

--- Syd/Syd/Program.cs
+++ Syd/Syd/Program.cs
@@ -12,11 +12,7 @@ namespace Syd
     {
         static void Main(string[] args)
         {
-<<<<<<< HEAD
             DownloadVideo("https://www.youtube.com/watch?v=0KSOMA3QBU0&list=PLMC9KNkIncKtPzgY-5rmhvj7fax8fdxoj", @"D:\projects\Syd");
-=======
-            DownloadVideo("https://www.youtube.com/watch?v=xm41dHucxmM", @"C:\Users\Omer\Downloads");
->>>>>>> 01a5b3fcf78547547172ca9d29468c68ca457feb
         }
         static void DownloadVideo(string link, string dir)
         {
@@ -24,13 +20,8 @@ namespace Syd
             var video = youTube.GetVideo(link);
             Console.WriteLine("got");
             string file = dir + @"\" + video.FullName;
-<<<<<<< HEAD
             file = Path.ChangeExtension(file, ".mp3");
             File.WriteAllBytes(file, video.GetBytes());
-=======
-            System.IO.File.WriteAllBytes(file, video.GetBytes());
-            Path.ChangeExtension(file, ".mp3");
->>>>>>> 01a5b3fcf78547547172ca9d29468c68ca457feb
         }
     }
 }
