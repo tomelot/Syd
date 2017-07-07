14db0f340724b5d8b26f6e835a818eea2b9903ca
omerAF <amiadomer@gmail.com>
2017-07-08 02:04:12 +0300

    overwriting tom

--- Syd/Syd/Program.cs
+++ Syd/Syd/Program.cs
@@ -6,23 +6,36 @@ using System.Text;
 using System.Threading.Tasks;
 using VideoLibrary;
 using TagLib;
+
 namespace Syd
 {
     class Program
     {
         static void Main(string[] args)
         {
+            string file;
+            file=DownloadVideo("https://www.youtube.com/watch?v=xm41dHucxmM", @"C:\Users\Omer\Downloads");
+            System.IO.File.Move(file, Path.GetDirectoryName(file) + "dick" + ".mp3");
+            Console.WriteLine(file);
+            ChangeDescription(file);
 
         }
-        static void DownloadVideo(string link, string dir)
+        static string DownloadVideo(string link, string dir)
         {
             var youTube = YouTube.Default;
             var video = youTube.GetVideo(link);
-            Console.WriteLine("got");
             string file = dir + @"\" + video.FullName;
             file = Path.ChangeExtension(file, ".mp3");
             System.IO.File.WriteAllBytes(file, video.GetBytes());
-            Console.WriteLine(video.Stream().ReadByte().ToString());
+            return file;
+        }
+        static void ChangeDescription(string path)
+        {
+            TagLib.File f = TagLib.File.Create(path);
+            f.Tag.Album = "omer";
+            //file.Tag.Performers = new string[] { artist };
+            f.Save();
+            //System.IO.File.Move(dir, Path.GetDirectoryName(dir)+name+".mp3");
         }
     }
 }
