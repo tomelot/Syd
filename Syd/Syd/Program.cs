a29e4a41c4992f81f25f83ff02c01fd1ea80570c
tomelot <tomlot2012@gmail.com>
2017-07-08 01:04:55 +0300

    tom
    ds

--- Syd/Syd/Program.cs
+++ Syd/Syd/Program.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using VideoLibrary;
 
 namespace Syd
 {
@@ -11,7 +10,8 @@ namespace Syd
     {
         static void Main(string[] args)
         {
-            
+            Console.WriteLine("OMER");
+
         }
     }
 }
