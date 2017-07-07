02c60793e48f4c3f5120a2cdbccc0a6e1b6693fb
tomelot <tomlot2012@gmail.com>
2017-07-08 01:35:11 +0300

    ghh

--- Syd/Syd/Program.cs
+++ Syd/Syd/Program.cs
@@ -5,7 +5,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using VideoLibrary;
-using TagLib;
 
 namespace Syd
 {
@@ -13,26 +12,16 @@ namespace Syd
     {
         static void Main(string[] args)
         {
-<<<<<<< HEAD
-            DownloadVideo("https://www.youtube.com/watch?v=xm41dHucxmM", @"D:\projects\Syd");
-=======
             DownloadVideo("https://www.youtube.com/watch?v=0KSOMA3QBU0&list=PLMC9KNkIncKtPzgY-5rmhvj7fax8fdxoj", @"D:\projects\Syd");
->>>>>>> tom
         }
         static void DownloadVideo(string link, string dir)
         {
             var youTube = YouTube.Default;
             var video = youTube.GetVideo(link);
-<<<<<<< HEAD
-            string file = dir + @"\" + video.FullName;
-            File.WriteAllBytes(file, video.GetBytes());
-            Path.ChangeExtension(file, ".mp3");
-=======
             Console.WriteLine("got");
             string file = dir + @"\" + video.FullName;
             file = Path.ChangeExtension(file, ".mp3");
             File.WriteAllBytes(file, video.GetBytes());
->>>>>>> tom
         }
     }
 }
