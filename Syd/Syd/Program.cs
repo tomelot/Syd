9c9e30a62161757665a1cc948ac1a29d285539d8
omerAF <amiadomer@gmail.com>
2017-07-08 01:09:41 +0300

    added download function

--- Syd/Syd/Program.cs
+++ Syd/Syd/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,12 @@ namespace Syd
         static void Main(string[] args)
         {
 
+        }
+        static void DownloadVideo(string link, string dir)
+        {
+            var youTube = YouTube.Default;
+            var video = youTube.GetVideo(link);
+            File.WriteAllBytes(dir + video.FullName, video.GetBytes());
         }
     }
 }
