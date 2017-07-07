536bd3416679348137cf60d175098683c2dc4f6f
omerAF <amiadomer@gmail.com>
2017-07-08 02:54:33 +0300

    can change description now

--- Syd/Syd/Program.cs
+++ Syd/Syd/Program.cs
@@ -13,10 +13,8 @@ namespace Syd
     {
         static void Main(string[] args)
         {
-            string file;
-            file=DownloadVideo("https://www.youtube.com/watch?v=IjV9Q9ujIEI", @"C:\Users\Omer\Downloads");
-            //Console.WriteLine(file);
-            //ChangeDescription(file, "omern","3124","12312");
+            string file = DownloadVideo("https://www.youtube.com/watch?v=IjV9Q9ujIEI", @"C:\Users\Omer\Downloads");
+            ChangeDescription(file, "song name","song artist","song album");
 
         }
         static string DownloadVideo(string link, string dir)
@@ -24,17 +22,17 @@ namespace Syd
             var youTube = YouTube.Default;
             var video = youTube.GetVideo(link);
             string file = dir + @"\" + video.FullName;
-            file = Path.ChangeExtension(file, ".wav");
-            file = Path.ChangeExtension(file, ".mp3");
             System.IO.File.WriteAllBytes(file, video.GetBytes());
             return file;
         }
         static void ChangeDescription(string path,string name, string artist, string album)
         {
             TagLib.File f = TagLib.File.Create(path);
-            //f.Tag.Performers = new string[] { artist };
-            //f.Save();
-            //System.IO.File.Move(dir, Path.GetDirectoryName(dir)+name+".mp3");
+            f.Tag.Album = album;
+            f.Tag.Title = name;
+            f.Tag.Performers = new string[] { artist };
+            f.Save();
+            System.IO.File.Move(path, Path.GetDirectoryName(path)+@"\"+name+".mp3");
         }
     }
 }
