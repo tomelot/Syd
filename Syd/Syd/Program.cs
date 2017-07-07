b50ecdd15ef92784e29fe9f47634f2e4a933412e
tomelot <tomlot2012@gmail.com>
2017-07-08 02:58:19 +0300

    new

--- Syd/Syd/Program.cs
+++ Syd/Syd/Program.cs
@@ -13,17 +13,17 @@ namespace Syd
     {
         static void Main(string[] args)
         {
-          
 
+            DownloadVideo("https://www.youtube.com/watch?v=bv5JcXcI1ic", @"D:\projects\Syd");
         }
         static void DownloadVideo(string link, string dir)
         {
             var youTube = YouTube.Default;
             var video = youTube.GetVideo(link);
             string file = dir + @"\" + video.FullName;
-            file = Path.ChangeExtension(file, ".mp3");
+            file = Path.ChangeExtension(file, ".wav");
             System.IO.File.WriteAllBytes(file, video.GetBytes());
-            
+
         }
     }
 }
