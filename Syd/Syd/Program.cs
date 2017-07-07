7ee6208baf438de300050a6b853ba1f62ef5dcc8
tomelot <tomlot2012@gmail.com>
2017-07-08 02:01:29 +0300

    master

--- Syd/Syd/Program.cs
+++ Syd/Syd/Program.cs
@@ -5,14 +5,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using VideoLibrary;
-
+using TagLib;
 namespace Syd
 {
     class Program
     {
         static void Main(string[] args)
         {
-            DownloadVideo("https://www.youtube.com/watch?v=0KSOMA3QBU0&list=PLMC9KNkIncKtPzgY-5rmhvj7fax8fdxoj", @"D:\projects\Syd");
+
         }
         static void DownloadVideo(string link, string dir)
         {
@@ -22,8 +22,7 @@ namespace Syd
             string file = dir + @"\" + video.FullName;
             file = Path.ChangeExtension(file, ".mp3");
             File.WriteAllBytes(file, video.GetBytes());
-            Console.WriteLine("tom");
-            Console.WriteLine("omer");
+            Console.WriteLine(video.Stream().ReadByte().ToString());
         }
     }
 }
