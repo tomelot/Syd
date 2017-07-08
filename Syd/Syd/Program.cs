81c71c496f824d8870de8f97c5735e2faeebfddd
tomelot <tomlot2012@gmail.com>
2017-07-09 02:44:21 +0300

    more changes

--- Syd/Syd/Program.cs
+++ Syd/Syd/Program.cs
@@ -18,133 +18,15 @@ namespace Syd
 {
     class Program
     {
-        static List<TcpClient> clients = new List<TcpClient>();
+       
         static void Main(string[] args)
         {
-            CreateServer();
             string FileName = DownloadAudio("https://www.youtube.com/watch?v=IjV9Q9ujIEI", @"D:\projects\Syd");
            // ChangeDescription(FileName, "song name","song artist","song album");
             //Console.WriteLine("done");
             //Console.ReadKey();
         }
-        static void ServerSendToAll(string data)
-        {
-            foreach (TcpClient client in clients)
-            {
-                NetworkStream stream = client.GetStream();
-                byte[] msg = System.Text.Encoding.ASCII.GetBytes(data);
-                stream.Write(msg, 0, msg.Length);
-            }
-        }
-        static void ServerSendToOne(string data,string clientip)
-        {
-            foreach (TcpClient client in clients)
-            {
-                string clientIPAddress = IPAddress.Parse(((IPEndPoint)client.Client.RemoteEndPoint).Address.ToString()).ToString();
-                if (clientIPAddress == clientip)
-                {
-                    NetworkStream stream = client.GetStream();
-                    byte[] msg = System.Text.Encoding.ASCII.GetBytes(data);
-                    stream.Write(msg, 0, msg.Length);
-                }
-            }
-        }
-        static void ServerRecive()
-        {
-            Byte[] bytes = new Byte[256];
-            String data = null;
-            int i;
-            foreach (TcpClient client in clients)
-            {
-                NetworkStream stream = client.GetStream();
-                i = stream.Read(bytes, 0, bytes.Length);
-                if (i != 0)
-                {
-                    data = System.Text.Encoding.ASCII.GetString(bytes, 0, i);
-                }
-                switch (data.Split(':')[0])
-                {
-
-                }
-            }
-        }
-        static void ClientRecive(TcpClient client)
-        {
-            while (true)
-            {
-                Byte[] bytes = new Byte[256];
-                String data = null;
-                int i;
-                NetworkStream stream = client.GetStream();
-                i = stream.Read(bytes, 0, bytes.Length);
-                if (i != 0)
-                {
-                    data = System.Text.Encoding.ASCII.GetString(bytes, 0, i);
-                }
-                switch (data.Split(':')[0])
-                {
-
-                }
-            }
-        }
-        static void CreateServer()
-        {
-            Int32 port = 25565;
-            IPAddress localAddr = IPAddress.Parse(GetComputer_LanIP());
-            TcpListener server = new TcpListener(localAddr, port);
-            server.Start();
-            Thread myNewThread = new Thread(() => WaitForClients(server));
-            myNewThread.Start();
-            Console.WriteLine(GetComputer_InternetIP());
-        }
-        private static void WaitForClients(TcpListener server)
-        {
-            while (true)
-            {
-                TcpClient client = server.AcceptTcpClient();
-                clients.Add(client);
-                Console.WriteLine("Conected to: " + IPAddress.Parse(((IPEndPoint)client.Client.RemoteEndPoint).Address.ToString()));
-            }
-        }
-        static void ClientConnect()
-        {
-            String ip = Console.ReadLine();
-            TcpClient client = new TcpClient();
-            client.Connect(ip, 25565);
-           
-
-        }
-        static string GetComputer_LanIP()
-        {
-            string strHostName = System.Net.Dns.GetHostName();
-
-            IPHostEntry ipEntry = System.Net.Dns.GetHostEntry(strHostName);
-
-            foreach (IPAddress ipAddress in ipEntry.AddressList)
-            {
-                if (ipAddress.AddressFamily.ToString() == "InterNetwork")
-                {
-                    return ipAddress.ToString();
-                }
-            }
-
-            return null;
-        }
-        static string GetComputer_InternetIP()
-        {
-            // check IP using DynDNS's service
-            WebRequest request = WebRequest.Create("http://checkip.dyndns.org");
-            WebResponse response = request.GetResponse();
-            StreamReader stream = new StreamReader(response.GetResponseStream());
-
-            // read complete response
-            string ipAddress = stream.ReadToEnd();
-
-            // replace everything and keep only IP
-            return ipAddress.
-                Replace("<html><head><title>Current IP Check</title></head><body>Current IP Address: ", string.Empty).
-                Replace("</body></html>", string.Empty);
-        }
+       
         //Download video from YouTube and convert it to mp3
         static string DownloadAudio(string link, string dir)
         {
