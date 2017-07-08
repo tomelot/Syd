8224273903d5bc59d2b00f617493b6439a1111af
omerAF <amiadomer@gmail.com>
2017-07-08 14:45:22 +0300

    added MP4toMP3 function

--- Syd/Syd/Program.cs
+++ Syd/Syd/Program.cs
@@ -4,8 +4,11 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics;
 using VideoLibrary;
 using TagLib;
+using MediaToolkit;
+using MediaToolkit.Model;
 
 namespace Syd
 {
@@ -13,17 +16,16 @@ namespace Syd
     {
         static void Main(string[] args)
         {
-            string file = DownloadVideo("https://www.youtube.com/watch?v=IjV9Q9ujIEI", @"C:\Users\Omer\Downloads");
-            ChangeDescription(file, "song name","song artist","song album");
-
+            string FileName = DownloadAudio("https://www.youtube.com/watch?v=IjV9Q9ujIEI", @"C:\Users\Omer\Downloads");
+            ChangeDescription(FileName, "song name","song artist","song album");
         }
-        static string DownloadVideo(string link, string dir)
+        static string DownloadAudio(string link, string dir)
         {
             var youTube = YouTube.Default;
             var video = youTube.GetVideo(link);
-            string file = dir + @"\" + video.FullName;
-            System.IO.File.WriteAllBytes(file, video.GetBytes());
-            return file;
+            string path = dir + @"\" + video.FullName;
+            System.IO.File.WriteAllBytes(path, video.GetBytes());
+            return MP4toMP3(path);
         }
         static void ChangeDescription(string path,string name, string artist, string album)
         {
@@ -33,6 +35,18 @@ namespace Syd
             f.Tag.Performers = new string[] { artist };
             f.Save();
             System.IO.File.Move(path, Path.GetDirectoryName(path)+@"\"+name+".mp3");
+        }
+        static string MP4toMP3(string mp4)
+        {
+            string NewFileName = Path.ChangeExtension(mp4, ".mp3");
+            var inputFile = new MediaFile { Filename = mp4 };
+            var outputFile = new MediaFile { Filename = NewFileName };
+            using (var engine = new Engine())
+            {
+                engine.Convert(inputFile, outputFile);
+            }
+            System.IO.File.Delete(mp4);
+            return NewFileName;
         }
     }
 }
